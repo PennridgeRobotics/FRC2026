86ae754e59dccacbcf0c7fe027e80a192784bc95
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2026-02-24 01:51:24 -0500

    bunch of fuel fixes

--- README.md
+++ README.md
@@ -24,21 +24,22 @@ Note: Ordered by priority
 | Passive              | Dark Green (or white if low time) |
 
 ## CAN IDs
-| Device                     | CAN ID |
-|----------------------------|--------|
-| Front-Left Drive Motor     | 1      |
-| Front-Left Steering Motor  | 2      |
-| Front-Right Drive Motor    | 3      |
-| Front-Right Steering Motor | 4      |
-| Back-Right Drive Motor     | 5      |
-| Back-Right Steering Motor  | 6      |
-| Back-Left Drive Motor      | 7      |
-| Back-Left Steering Motor   | 8      |
-| Climber Motor              | 9      |
-| Pigeon 2                   | 13     |
-| Intake/Launcher Motor      | 14     |
-| Feeder Motor               | 15     |
-| CANdle (LEDs)              | 16     |
+| Device                      | CAN ID |
+|-----------------------------|--------|
+| Front-Left Drive Motor      | 1      |
+| Front-Left Steering Motor   | 2      |
+| Front-Right Drive Motor     | 3      |
+| Front-Right Steering Motor  | 4      |
+| Back-Right Drive Motor      | 5      |
+| Back-Right Steering Motor   | 6      |
+| Back-Left Drive Motor       | 7      |
+| Back-Left Steering Motor    | 8      |
+| Climber Motor               | 9      |
+| Intake/Launcher Left Motor  | 10     |
+| Intake/Launcher Right Motor | 11     |
+| Feeder Motor                | 12     |
+| Pigeon 2                    | 13     |
+| CANdle (LEDs)               | 16     |
 
 ## Development
 Here are some things to keep in mind while working on this codebase:
--- src/main/java/frc/robot/util/enums/Constants.java
+++ src/main/java/frc/robot/util/enums/Constants.java
@@ -108,8 +108,22 @@ public final class Constants {
     public static final class FuelConstants {
         public static final boolean FUEL_SUBSYSTEM_ENABLED = false;
 
-        public static final int INTAKE_LAUNCHER_MOTOR_ID = 14;
-        public static final int FEEDER_MOTOR_ID = 15;
+        public static final int INTAKE_LAUNCHER_LEFT_MOTOR_ID = 10;
+        public static final int INTAKE_LAUNCHER_RIGHT_MOTOR_ID = 11;
+        public static final int FEEDER_MOTOR_ID = 12;
+
+        public static final boolean INTAKE_LAUNCHER_INVERTED = false;
+        public static final boolean FEEDER_INVERTED = false;
+        public static final Voltage INTAKE_LAUNCHER_VOLTAGE_COMP = Volts.of(12);
+        public static final Voltage FEEDER_VOLTAGE_COMP = Volts.of(12);
+        public static final Current INTAKE_LAUNCHER_CURRENT_LIMIT = Amps.of(40);
+        public static final Current FEEDER_CURRENT_LIMIT = Amps.of(40);
+        public static final Time INTAKE_LAUNCHER_RAMP_RATE = Seconds.of(0.2);
+        public static final Time FEEDER_RAMP_RATE = Seconds.of(0.2);
+        public static final MotorMode INTAKE_LAUNCHER_MOTOR_MODE = MotorMode.BRAKE;
+        public static final MotorMode FEEDER_MOTOR_MODE = MotorMode.BRAKE;
+        public static final MechanismGearing INTAKE_LAUNCHER_GEARING = new MechanismGearing(60.0 / 40);
+        public static final MechanismGearing FEEDER_GEARING = new MechanismGearing(32.0 / 18);
 
         public static final Current MOTOR_CURRENT_LIMIT = Amps.of(40);
 
