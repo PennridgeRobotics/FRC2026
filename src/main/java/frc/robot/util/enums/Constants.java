ec35d21dec8f0807ac132684b3201c5506f15e9c
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2026-03-25 20:58:55 -0400

    set unknown values to 0

--- src/main/java/frc/robot/util/enums/Constants.java
+++ src/main/java/frc/robot/util/enums/Constants.java
@@ -64,8 +64,8 @@ public final class Constants {
         public static final Rotation3d CAMERA_BACK_ROTATION = new Rotation3d(-0.007874, 0.014070, -3.123649);
         // Front Camera
         public static final String CAMERA_FRONT_NAME = "Arducam_OV9281_Front";
-        public static final Translation3d CAMERA_FRONT_TRANSLATION = new Translation3d(0.162087, -0.250706, 0.674197);
-        public static final Rotation3d CAMERA_FRONT_ROTATION = new Rotation3d(-0.007874, 0.014070, -3.123649);
+        public static final Translation3d CAMERA_FRONT_TRANSLATION = new Translation3d(0.0, 0.0, 0.0);
+        public static final Rotation3d CAMERA_FRONT_ROTATION = new Rotation3d(0.0, 0.0, 0.0);
     }
 
     public static final class DriveConstants {
