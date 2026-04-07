0cf7fc11f3c98ac9a5fae278a568bce5738a2466
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2026-04-07 01:41:37 -0400

    refactor some fuel logging and auto max power

--- src/main/java/frc/robot/util/dashboard/LoggedNetworkUnit.java
+++ src/main/java/frc/robot/util/dashboard/LoggedNetworkUnit.java
@@ -7,13 +7,21 @@ import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 public class LoggedNetworkUnit<U extends Unit, T extends Measure<U>> extends LoggedNetworkDoubleToObject<T> {
-    @SuppressWarnings("unchecked")
     public LoggedNetworkUnit(String rawTopicName, T defaultValue) {
-        super(rawTopicName, defaultValue, num -> (T) defaultValue.unit().of(num), obj -> obj.in(defaultValue.unit()));
+        this(rawTopicName, defaultValue, defaultValue.unit());
+    }
+
+    @SuppressWarnings("unchecked")
+    public LoggedNetworkUnit(String rawTopicName, T defaultValue, U unit) {
+        super(rawTopicName, defaultValue, num -> (T) unit.of(num), obj -> obj.in(unit));
     }
 
     public LoggedNetworkUnit(String rawTopicName, Supplier<T> supplier) {
-        this(rawTopicName, supplier.get());
+        this(rawTopicName, supplier.get(), supplier.get().unit());
+    }
+
+    public LoggedNetworkUnit(String rawTopicName, Supplier<T> supplier, U unit) {
+        this(rawTopicName, supplier.get(), unit);
         setSupplier(supplier);
     }
 }
