843ccd0206a7c8a34e2f1526a155dc49760672f1
CatsCanCoder <catgamermc711@gmail.com>
2026-04-12 21:07:22 -0400

    Update flip_path.py again
    Lets the user cancel an operation at any stage.

--- src/main/deploy/autos/paths/flip_path.py
+++ src/main/deploy/autos/paths/flip_path.py
@@ -75,6 +75,9 @@ while True:
             if cancel: break
             new_file = input("New name of path: ")
             if "\\" in new_file or"/" in new_file or ":" in new_file or "*" in new_file or "?" in new_file or '"' in new_file or "<" in new_file or ">" in new_file or "|" in new_file or new_file.lower() == "cancel":
+                if new_file.lower() == "cancel":
+                    cancel = True
+                    break
                 print('These symbols cannot be in the filename: \\ / : * ? " < > | ')
                 print("'cancel' and ' ' cannot be in the filename either.\n")
             else:
@@ -174,115 +177,118 @@ while True:
                     if mode == 'v' or mode == 'h' or mode == 'd':
                         valid = True
                     else:
+                        if mode.lower() == "cancel":
+                            cancel = True
+                            break
                         print("That mode does not exist!\n")
 
-                    
-            index = 0
-            for file in jsons:
-                for index in range(3):
+            if not cancel:                    
+                index = 0
+                for file in jsons:
+                    for index in range(3):
 
-                    if cmd == "auto":
-                        if index == 0:
-                            mode = 'v'
-                        elif index == 1:
-                            mode = 'h'
-                        elif index == 2:
-                            mode = 'v'
-                            file_add = ""
-                            if "-a" in file:
-                                file_add = "-a"
-                            elif "-b" in file:
-                                file_add = "-b"
-                            if "_blue" in file:
-                                file = file.split("_blue")[0] + "_red" + file_add + ".json"
-                            elif "_red" in file:
-                                file = file.split("_red")[0] + "_red" + file_add + ".json"
-                            else:
-                                file = file.split(".json")[0] + " (3)"  + ".json"
-                                
-                    index_flip_check = index == 1 and cmd == "flip"
-                    if index_flip_check or cmd == "auto":
-                        with open(file, 'r') as path:
-                            path_r_split = path.read().split(" ")
-                            path_r_split_temp = []
-                            raw_data = []
-                            for item in path_r_split:
-                                path_r_split_temp.append(item.split('\n')[0])
-                            for item in  path_r_split_temp:
-                                raw_data.append(item.split(',')[0])
-
-                            if mode == 'v' or mode == 'd':
-                                for i, item in enumerate(raw_data):
-                                    item_add = ""
-                                    if i < len(raw_data) - 1:
-                                        if not path_r_split[i+1] == '}\n':
-                                             item_add = ',\n'
-                                        else:
-                                            item_add = '\n'
+                        if cmd == "auto":
+                            if index == 0:
+                                mode = 'v'
+                            elif index == 1:
+                                mode = 'h'
+                            elif index == 2:
+                                mode = 'v'
+                                file_add = ""
+                                if "-a" in file:
+                                    file_add = "-a"
+                                elif "-b" in file:
+                                    file_add = "-b"
+                                if "_blue" in file:
+                                    file = file.split("_blue")[0] + "_red" + file_add + ".json"
+                                elif "_red" in file:
+                                    file = file.split("_red")[0] + "_red" + file_add + ".json"
+                                else:
+                                    file = file.split(".json")[0] + " (3)"  + ".json"
                                     
-                                    if item == '"y_meters":':
-                                        path_r_split[i+1] = str( 8.1858 - float(raw_data[i+1]) ) + item_add
+                        index_flip_check = index == 1 and cmd == "flip"
+                        if index_flip_check or cmd == "auto":
+                            with open(file, 'r') as path:
+                                path_r_split = path.read().split(" ")
+                                path_r_split_temp = []
+                                raw_data = []
+                                for item in path_r_split:
+                                    path_r_split_temp.append(item.split('\n')[0])
+                                for item in  path_r_split_temp:
+                                    raw_data.append(item.split(',')[0])
 
-                                    # deg_change = deg - 180; deg = 180 - deg_change (3.141593rad = 180deg)
-                                    if item == '"rotation_radians":':
-                                        path_r_split[i+1] = str( 3.141593 - (float(raw_data[i+1]) - 3.141593) ) + item_add
-
-                                if not mode == 'd':
-                                    new_file = file.split(".json")[0].split(f"_({index+2-1})")[0] + f"_({index+2})"  + ".json"
-                                    if "-a" in file:
-                                        new_file = file.split("-a")[0]  + "-b" + ".json"
-                                    elif "-b" in file:
-                                        new_file = file.split("-b")[0]  + "-a" + ".json"
-
-                                                
-                            if mode == 'h' or mode == 'd':
-                                for i, item in enumerate(raw_data):
-                                    item_add = ""
-                                    if i < len(raw_data)-1:
-                                        if not path_r_split[i+1] == '}\n':
-                                            item_add = ',\n'
-                                        else:
-                                            item_add = '\n'
-                                    
-                                    if item == '"x_meters":':
-                                        path_r_split[i+1] = str( 16.7193 - float(raw_data[i+1]) ) + item_add
-
-                                # deg = deg - 180 (3.141593rad = 180deg)
-                                    if item == '"rotation_radians":':
-                                        path_r_split[i+1] = str( 3.141593 - float(raw_data[i+1]) ) + item_add
+                                if mode == 'v' or mode == 'd':
+                                    for i, item in enumerate(raw_data):
+                                        item_add = ""
+                                        if i < len(raw_data) - 1:
+                                            if not path_r_split[i+1] == '}\n':
+                                                 item_add = ',\n'
+                                            else:
+                                                item_add = '\n'
                                         
-                                if not mode == 'd':
+                                        if item == '"y_meters":':
+                                            path_r_split[i+1] = str( 8.1858 - float(raw_data[i+1]) ) + item_add
+
+                                        # deg_change = deg - 180; deg = 180 - deg_change (3.141593rad = 180deg)
+                                        if item == '"rotation_radians":':
+                                            path_r_split[i+1] = str( 3.141593 - (float(raw_data[i+1]) - 3.141593) ) + item_add
+
+                                    if not mode == 'd':
+                                        new_file = file.split(".json")[0].split(f"_({index+2-1})")[0] + f"_({index+2})"  + ".json"
+                                        if "-a" in file:
+                                            new_file = file.split("-a")[0]  + "-b" + ".json"
+                                        elif "-b" in file:
+                                            new_file = file.split("-b")[0]  + "-a" + ".json"
+
+                                                    
+                                if mode == 'h' or mode == 'd':
+                                    for i, item in enumerate(raw_data):
+                                        item_add = ""
+                                        if i < len(raw_data)-1:
+                                            if not path_r_split[i+1] == '}\n':
+                                                item_add = ',\n'
+                                            else:
+                                                item_add = '\n'
+                                        
+                                        if item == '"x_meters":':
+                                            path_r_split[i+1] = str( 16.7193 - float(raw_data[i+1]) ) + item_add
+
+                                    # deg = deg - 180 (3.141593rad = 180deg)
+                                        if item == '"rotation_radians":':
+                                            path_r_split[i+1] = str( 3.141593 - float(raw_data[i+1]) ) + item_add
+                                            
+                                    if not mode == 'd':
+                                        new_file = file.split(".json")[0].split(f"_({index+2-1})"  + ".json")[0] + f"_({index+2})"  + ".json"
+                                        file_add = ""
+                                        if "-a" in file:
+                                            file_add = "-a"
+                                        elif "-b" in file:
+                                            file_add = "-b"
+                                        if "_blue" in file:
+                                            new_file = file.split("_blue")[0] + "_red" + file_add + ".json"
+                                        elif "_red" in file:
+                                            new_file = file.split("_red")[0] + "_red" + file_add + ".json"
+
+                                if mode == 'd':
                                     new_file = file.split(".json")[0].split(f"_({index+2-1})"  + ".json")[0] + f"_({index+2})"  + ".json"
                                     file_add = ""
                                     if "-a" in file:
-                                        file_add = "-a"
-                                    elif "-b" in file:
                                         file_add = "-b"
+                                    elif "-b" in file:
+                                        file_add = "-a"
                                     if "_blue" in file:
                                         new_file = file.split("_blue")[0] + "_red" + file_add + ".json"
                                     elif "_red" in file:
                                         new_file = file.split("_red")[0] + "_red" + file_add + ".json"
 
-                            if mode == 'd':
-                                new_file = file.split(".json")[0].split(f"_({index+2-1})"  + ".json")[0] + f"_({index+2})"  + ".json"
-                                file_add = ""
-                                if "-a" in file:
-                                    file_add = "-b"
-                                elif "-b" in file:
-                                    file_add = "-a"
-                                if "_blue" in file:
-                                    new_file = file.split("_blue")[0] + "_red" + file_add + ".json"
-                                elif "_red" in file:
-                                    new_file = file.split("_red")[0] + "_red" + file_add + ".json"
 
-
-                            with open(new_file, 'w') as path:
-                                file_contents = ""
-                                for item in path_r_split:
-                                    file_contents += item + ' '
-                                    
-                                path.write(file_contents)
-                                print(f'File made: "{new_file}"')
+                                with open(new_file, 'w') as path:
+                                    file_contents = ""
+                                    for item in path_r_split:
+                                        file_contents += item + ' '
+                                        
+                                    path.write(file_contents)
+                                    print(f'File made: "{new_file}"')
                 
     print("")
 
