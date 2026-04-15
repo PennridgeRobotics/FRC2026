62cf698ea2e87254fafa7f096b8f357af53bb814
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2026-04-15 18:54:07 -0400

    fix camera

--- src/main/java/frc/robot/vision/Camera.java
+++ src/main/java/frc/robot/vision/Camera.java
@@ -8,6 +8,7 @@ import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.util.dashboard.LoggedNetworkBoolean;
@@ -19,7 +20,8 @@ import org.jspecify.annotations.Nullable;
 @NullMarked
 public abstract class Camera {
 
-    private static final NetworkTableInstance networkTableInstance = NetworkTableInstance.create();
+    private static final NetworkTableInstance networkTableInstance =
+            RobotBase.isReal() ? NetworkTableInstance.getDefault() : NetworkTableInstance.create();
     private static final DoublePublisher stdDevsPublisher = NetworkTableInstance.getDefault()
             .getDoubleTopic("Vision/All Standard Deviations")
             .publish();
